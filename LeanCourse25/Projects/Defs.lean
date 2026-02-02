b77f29e40899236eb822e0afe73632a3cf7bb8c0
Lasse Lenzing <lasse.lenzing@proton.me>
2026-02-02 18:02:39 +0100

    18:02

--- LeanCourse25/Projects/Defs.lean
+++ LeanCourse25/Projects/Defs.lean
@@ -5,9 +5,9 @@ import Mathlib.Logic.Equiv.Fin.Basic
 
 open Classical SimpleGraph
 
-variable {α : Type} [Fintype α]
-variable {roots : Finset α}
-variable {G : SimpleGraph α}
+--there is exactly one element from roots in each connected component of G
+variable {α : Type} [Fintype α] {w : α} {roots : Finset α} {G : SimpleGraph α}
+  {hw : w ∈ roots}
 
 def is_forest_with_roots_in_set {α : Type} [Fintype α] (G : SimpleGraph α)
   (roots : Finset α) : Prop :=
@@ -22,18 +22,19 @@ class IsForestWithRoots (G : SimpleGraph α) (roots : Finset α) : Prop where
 variable {hG : G ∈ forest_set roots}
 instance : IsForestWithRoots G roots := ⟨hG⟩
 
-def induce_hom (w : α) (S : SimpleGraph α) : S.induce {v | v ≠ w} →g S := {
+def induce_hom (S : SimpleGraph α) : S.induce {v | v ≠ w} →g S := {
   toFun := fun v => v.val
   map_rel' := by intro v w h; exact h}
 
-lemma induce_hom_inj (w : α) (S : SimpleGraph α) : Function.Injective (induce_hom w S)  := by
+omit [Fintype α] in
+lemma induce_hom_inj (S : SimpleGraph α) : Function.Injective (induce_hom (w:= w) S)  := by
   intro v w h
   exact Subtype.ext h
 
-def delete_vertex (w : α) (G : SimpleGraph α) : SimpleGraph {v | v ≠ w} :=
+def f (G : SimpleGraph α) : SimpleGraph {v | v ≠ w} :=
   G.induce {v | v ≠ w}
 
-noncomputable def new_roots (roots : Finset α) (w : α) (G : SimpleGraph α) :
+noncomputable def new_roots (roots : Finset α) (G : SimpleGraph α) :
   Finset {v | v ≠ w} := (roots ∪ G.neighborFinset w).subtype (fun v => v ≠ w)
 
 
@@ -61,7 +62,7 @@ lemma roots_not_reachable [IsForestWithRoots G roots] :
   by_contra hc
   exact he (inj_comp u hu w hw hc)
 
-lemma roots_neighbors_not_reachable (w : α) (hw : w ∈ roots) [IsForestWithRoots G roots] :
+lemma roots_neighbors_not_reachable {hw : w ∈ roots} [IsForestWithRoots G roots] :
   ∀ u v, u ∈ roots → v ∈ G.neighborFinset w → u ≠ w → ¬G.Reachable u v := by
   intro u v hu hv he
   by_contra hc
@@ -70,7 +71,7 @@ lemma roots_neighbors_not_reachable (w : α) (hw : w ∈ roots) [IsForestWithRoo
     (Reachable.trans (Adj.reachable hv) (Reachable.symm hc))
 
 --otherwise there would be one path with w and one without w
-lemma neighbors_neighbors_not_reachable (w : α) (G : SimpleGraph α) (ha : G.IsAcyclic)
+lemma neighbors_neighbors_not_reachable (G : SimpleGraph α) (ha : G.IsAcyclic)
   (x y : α) (hx : x ≠ w) (hy : y ≠ w) : x ≠ y → x ∈ G.neighborFinset w → y ∈ G.neighborFinset w →
     ¬(G.induce {v | v ≠ w}).Reachable ⟨x, hx⟩ ⟨y, hy⟩ := by
   intro hc hx' hy'
@@ -82,7 +83,7 @@ lemma neighbors_neighbors_not_reachable (w : α) (G : SimpleGraph α) (ha : G.Is
   have pxwy : xwy.IsPath := by simp [xwy]; exact ⟨Ne.symm hy, hx, hc⟩
   have wp : w ∈ xwy.support := by simp [xwy]
   let xy : G.Path x y :=
-    ⟨p.map (induce_hom w G), Walk.map_isPath_of_injective (induce_hom_inj w G) hp⟩
+    ⟨p.map (induce_hom G), Walk.map_isPath_of_injective (induce_hom_inj G) hp⟩
   have wp' : w ∉ xy.1.support := by simp [xy, induce_hom]
   have hc : xy = ⟨xwy, pxwy⟩ := by apply IsAcyclic.path_unique ha
   have hc' : xy ≠ ⟨xwy, pxwy⟩ := by
@@ -93,7 +94,8 @@ lemma neighbors_neighbors_not_reachable (w : α) (G : SimpleGraph α) (ha : G.Is
 
 --if there is a walk W from x to y with w ∉ W.support,
 --there is a walk from x to y in G.induce {v | v ≠ w}
-lemma reachability_subgraph (x y : α) (p : G.Walk x y) (w : α) (hx : x ≠ w) (hy : y ≠ w)
+omit [Fintype α] in
+lemma reachability_subgraph (x y : α) (p : G.Walk x y) (hx : x ≠ w) (hy : y ≠ w)
   (hs : ∀ v ∈ p.support, v ≠ w) : (G.induce {v | v ≠ w}).Reachable ⟨x, hx⟩ ⟨y, hy⟩ :=
     ⟨by induction p with
       | nil => exact Walk.nil
@@ -109,8 +111,8 @@ lemma reachability_subgraph (x y : α) (p : G.Walk x y) (w : α) (hx : x ≠ w) 
 
 --prove that there is a path from every node x in G.induce {v | v ≠ w} to
 --a node in (roots \ {w} ∪ G.neighborFinset w)
-lemma new_roots_surj (w : α) (hw : w ∈ roots) [IsForestWithRoots G roots] :
-  ∀ x, ∃ y ∈ (new_roots roots w G), (G.induce {v | v ≠ w}).Reachable x y := by
+lemma new_roots_surj {hw : w ∈ roots} [IsForestWithRoots G roots] :
+  ∀ x, ∃ y ∈ (new_roots roots G), (G.induce {v | v ≠ w}).Reachable x y := by
   have hG := IsForestWithRoots.hG (G := G) (roots := roots)
   intro x
   let xcc : G.ConnectedComponent := G.connectedComponentMk x
@@ -132,7 +134,7 @@ lemma new_roots_surj (w : α) (hw : w ∈ roots) [IsForestWithRoots G roots] :
           rw [← hc]
           exact ⟨Walk.takeUntil p.reverse i (by simpa using hi)⟩
         exact (roots_not_reachable r w hr hw bc) hrw
-      exact reachability_subgraph x.1 r p w x.2 bc hps
+      exact reachability_subgraph x.1 r p x.2 bc hps
   · --case where x is in connected component of w
     rcases Reachable.exists_isPath hrr with ⟨p, hp⟩
     rw [not_ne_iff] at bc
@@ -149,14 +151,14 @@ lemma new_roots_surj (w : α) (hw : w ∈ roots) [IsForestWithRoots G roots] :
       have hrs : ∀ i ∈ p'.1.support, i ≠ r := by
         intro i hi
         exact ne_of_mem_of_not_mem hi vp'
-      exact reachability_subgraph x.1 n p'.1 r x.2 hne hrs
+      exact reachability_subgraph x.1 n p'.1 x.2 hne hrs
 
 
 --if we delete a root w, then we get a forest rooted in (roots \ {w} ∪ G.neighborFinset w)
-theorem maps_to (w : α) (hw : w ∈ roots) [IsForestWithRoots G roots] :
-  delete_vertex w G ∈ forest_set (new_roots roots w G) := by
+theorem f_maps_to {hw : w ∈ roots} [IsForestWithRoots G roots] :
+  f G ∈ forest_set (new_roots (w := w) roots G) := by
   have hG := IsForestWithRoots.hG (G := G) (roots := roots)
-  simp [delete_vertex, forest_set, is_forest_with_roots_in_set] at hG ⊢
+  simp [f, forest_set, is_forest_with_roots_in_set] at hG ⊢
   constructor
   · apply IsAcyclic.induce hG.1
   · simp [ConnectedComponent.Represents, Set.BijOn]
@@ -169,23 +171,22 @@ theorem maps_to (w : α) (hw : w ∈ roots) [IsForestWithRoots G roots] :
       rcases hx' with hx' | hx' <;>
       rcases hy' with hy' | hy'
       · exact (roots_not_reachable x y hx' hy' hc)
-          (Reachable.map (induce_hom w G) hr)
-      · exact roots_neighbors_not_reachable w hw x y hx' hy' hx
-          (Reachable.map (induce_hom w G) hr)
-      · exact roots_neighbors_not_reachable w hw y x hy' hx' hy
-          (Reachable.symm (Reachable.map (induce_hom w G) hr))
+          (Reachable.map (induce_hom G) hr)
+      · exact roots_neighbors_not_reachable (hw := hw) x y hx' hy' hx
+          (Reachable.map (induce_hom G) hr)
+      · exact roots_neighbors_not_reachable (hw := hw) y x hy' hx' hy
+          (Reachable.symm (Reachable.map (induce_hom G) hr))
       · have hh : ¬(induce {v | v ≠ w} G).Reachable ⟨x, hx⟩ ⟨y, hy⟩ := by
-          exact (neighbors_neighbors_not_reachable w G hG.1 x y hx hy hc hx' hy')
+          exact (neighbors_neighbors_not_reachable G hG.1 x y hx hy hc hx' hy')
         exact hh hr
-
     · simp [Set.SurjOn, Set.ext_iff, Set.mem_image, Set.mem_univ]
       intro cc
       rcases ConnectedComponent.nonempty_supp cc with ⟨d, hd⟩
       rw [ConnectedComponent.mem_supp_iff] at hd
       rw [← hd]
       simp only [ConnectedComponent.eq]
-
-
+      obtain ⟨a, ha, ha'⟩ := new_roots_surj (G := G) (w := w) (hw := hw) d
+      exact ⟨a.1, a.2, ha, Reachable.symm ha'⟩
 
 
 
